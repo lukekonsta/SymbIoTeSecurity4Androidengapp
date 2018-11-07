eff9f64fda47e8ee602cb9d23c75334c9d70bed2
Loukas <lukekonsta@gmail.com>
2018-11-07 15:16:58 +0200

    Changes

--- sampleapp/src/main/java/symbiote/h2020/eu/sampleapp/About.java
+++ sampleapp/src/main/java/symbiote/h2020/eu/sampleapp/About.java
@@ -17,14 +17,11 @@ public class About extends AppCompatActivity {
         txt = (TextView) findViewById(R.id.txt);
         String nodata = "This application was designed and developed by the “Media, Cognition and Learning” Research group and the IoT Lab of the Department of Communication and Internet Studies of the Cyprus University of Technology under the European research project “IoT for Education: Presenting air quality matters\"." +
                 "\n" +
-                "The project was funded by the European Union's Research and Innovation Program Horizon 2020 under the Grant Agreement No. 688156.";
+                "The project was funded by the European Union's Research and Innovation Program Horizon 2020 under the Grant Agreement No. 688156."
+                +"\n" +
+                "The air quality data for the city of Limassol are provided by the Air Quality Division of the Department of Labor Inspection of the Ministry of Labor, Welfare and Social Insurance of Cyprus.";;
 
         //txt.setText(Html.fromHtml(nodata));
-
-        String data2 = "The air quality data for the city of Limassol are provided by the Air Quality Division of the Department of Labor Inspection of the Ministry of Labor, Welfare and Social Insurance of Cyprus.";
-
-        txt2 = (TextView) findViewById(R.id.txt2);
-        //txt2.setText(data2);
 
 
     }
--- sampleapp/src/main/java/symbiote/h2020/eu/sampleapp/Map_Third.java
+++ sampleapp/src/main/java/symbiote/h2020/eu/sampleapp/Map_Third.java
@@ -123,7 +123,7 @@ public class Map_Third extends AppCompatActivity implements OnMapReadyCallback {
         CameraUpdate center=
                 CameraUpdateFactory.newLatLng(new LatLng(45.815011,
                         15.981919));
-        CameraUpdate zoom=CameraUpdateFactory.zoomTo(5);
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(7);
 
         mMap.moveCamera(center);
         mMap.animateCamera(zoom);
